ff0cffb8f01992d3becfc9ab8f2e501de0be1ae9
hidayat-shah <hidayatsha9@gmail.com>
2025-12-10 15:06:18 +0500

    corect logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import TopBar from "./TopBar";
 
 const Navbar = () => {
